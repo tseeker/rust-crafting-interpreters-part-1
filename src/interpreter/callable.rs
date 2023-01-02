34619a81253b1e9491a86e1df7dd42e4579cbe90
Emmanuel Benoît <tseeker@nocternity.net>
2023-01-02 17:40:06 +0100

    Interpreter - Callable extends Debug and ToString

--- src/interpreter/callable.rs
+++ src/interpreter/callable.rs
@@ -1,5 +1,11 @@
+use std::fmt::Debug;
+
+use crate::errors::InterpreterError;
+
+use super::{Environment, Value};
+
 /// A callable is some object that supports being called.
-pub trait Callable {
+pub trait Callable: Debug + ToString {
     /// Return the amount of arguments supported by the callable.
     fn arity(&self) -> usize;
 
