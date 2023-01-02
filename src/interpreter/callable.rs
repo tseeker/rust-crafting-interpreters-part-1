b1d0db5ea491889b316b5d73ae5e339b20018333
Emmanuel Benoît <tseeker@nocternity.net>
2023-01-02 15:54:50 +0100

    Interpreter - Callable interface definition

---
+++ src/interpreter/callable.rs
@@ -0,0 +1,13 @@
+/// A callable is some object that supports being called.
+pub trait Callable {
+    /// Return the amount of arguments supported by the callable.
+    fn arity(&self) -> usize;
+
+    /// Run the callable in the execution environment with the specified
+    /// arguments.
+    fn call(
+        &self,
+        environment: &mut Environment,
+        arguments: &Vec<Value>,
+    ) -> Result<Value, InterpreterError>;
+}
